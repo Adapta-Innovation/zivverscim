3e0914112b36c4ca544d44973e337587a22d3dc7
Igor van Spengen <igor.van.spengen@outlook.com>
2021-07-20 10:36:03 +0200

    Update wrapper, add alias and delegate

--- src/zivver/wrapper.py
+++ src/zivver/wrapper.py
@@ -35,8 +35,8 @@ def get_zivver_user_object(zivver_scim):
     zivver_scim_user_aliases = ''
     zivver_scim_user_delegates = ''
     if zivver_scum_user:
-        zivver_scim_user_aliases = zivver_scim.get('aliases', [])
-        zivver_scim_user_delegates = zivver_scim.get('delegates', [])
+        zivver_scim_user_aliases = zivver_scum_user.get('aliases', [])
+        zivver_scim_user_delegates = zivver_scum_user.get('delegates', [])
     
     return ZivverUser(account_id=account_id, name_formatted=name_formatted, meta_location=meta_location,
                       meta_created_at=meta_created_at, meta_resource_type=meta_resource_type,
